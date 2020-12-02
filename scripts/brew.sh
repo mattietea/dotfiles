162a4c203ec9e317086812e68a4c6d7067136eba
Matthew Thomas <mattcthomas@me.com>
2020-12-02 15:27:18 +0000

    feat: add support for x86_64 and arm

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-# Install homebrew
-sudo mkdir -p /opt/homebrew
-sudo chown -R $(whoami):staff /opt/homebrew
-cd /opt
-curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
+# Install homebrew
+if [[ $(uname -m) = 'arm64' ]]
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  sudo mkdir -p /opt/homebrew
+  sudo chown -R $(whoami):staff /opt/homebrew
+  cd /opt
+  curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
