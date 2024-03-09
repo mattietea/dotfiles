d4113f99c08ac7d5cdf1b3e14f1b0b235f9ce139
Matt Thomas <mattcthomas@me.com>
2024-03-09 08:52:59 +0000

    Update gitconfig

--- shell/env.zsh
+++ shell/env.zsh
@@ -2,5 +2,8 @@ alias c='clear'
 
 alias config="code ~/.dotfiles"
 alias reload="~/.dotfiles/install && source ~/.zshrc"
+alias how="gh copilot suggest how"
 
 [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+
+[ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
