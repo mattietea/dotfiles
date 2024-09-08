3a3fdc2d5da9fa11d4c4964e8ae287b74fa6a619
Matt Thomas <mattcthomas@me.com>
2024-09-08 16:22:51 +0100

    Add formatting

--- shell/rc.zsh
+++ shell/rc.zsh
@@ -33,4 +33,6 @@ export HISTFILE=~/.zsh_history
 touch $HISTFILE
 
 
+[[ -f /opt/dev/sh/chruby/chruby.sh ]] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; } }
 
+[[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
